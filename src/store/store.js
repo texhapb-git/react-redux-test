24ff21520a41ad845a4b0f2b1b548bdf7faaa473
TexHaPb <texhapb@bk.ru>
2022-12-19 14:16:22 +0300

    task delete

--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,9 @@ import { taskReducer } from "./taskReducer";
 const initialState = [
     { id: 1, title: "Task 1", completed: false },
     { id: 2, title: "Task 2", completed: false },
+    { id: 3, title: "Task 3", completed: false },
+    { id: 4, title: "Task 4", completed: false },
+    { id: 5, title: "Task 5", completed: false },
 ];
 
 export function initiateStore() {
